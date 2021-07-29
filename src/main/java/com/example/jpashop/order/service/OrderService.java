5b9e19ede307960b045fd64a363640da2222f52f
jwoojun <jjwjun10@gmail.com>
2021-07-30 07:55:10 +0900

    sync

--- src/main/java/com/example/jpashop/order/service/OrderService.java
+++ src/main/java/com/example/jpashop/order/service/OrderService.java
@@ -13,57 +13,63 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
-
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class OrderService {
 
+  private final OrderRepository orderRepository;
+  private final MemberRepository memberRepository;
+  private final ItemRepository itemRepository;
 
-    private final OrderRepository orderRepository;
-    private final MemberRepository memberRepository;
-    private final ItemRepository itemRepository;
+  @Transactional
+  public Long order(Long memberId, Long itemId, int count) {
 
+    // 회원, 아이템 정보 조회
+    Member member = memberRepository.findOne(memberId);
+    Item item = itemRepository.findOne(itemId);
 
-    @Transactional
-    public Long order(Long memberId, Long itemId, int count) {
+    // CaseCade는 Order가 관리하는 것.
+    // 배송정보 생성
+    Delivery delivery = new Delivery();
+    delivery.setAddress(member.getAddress());
 
+    // 주문상품 생성
+    OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
 
-        // 회원, 아이템 정보 조회
-        Member member = memberRepository.findOne(memberId);
-        Item item = itemRepository.findOne(itemId);
+    // 주문 생성
+    Order order = Order.createOrder(member, delivery, orderItem);
 
-        // CaseCade는 Order가 관리하는 것.
-        // 배송정보 생성
-        Delivery delivery = new Delivery();
-        delivery.setAddress(member.getAddress());
+    // 주문 저장
+    orderRepository.save(order);
 
-        // 주문상품 생성
-        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+    return order.getId();
+  }
 
-        // 주문 생성
-        Order order = Order.createOrder(member, delivery, orderItem);
-
-        // 주문 저장
-        orderRepository.save(order);
-
-        return order.getId();
-    }
-
-    @Transactional
-    public void cancelOrder(Long orderId) {
-        // 주문 엔티티 조회
-        Order order = orderRepository.findOne(orderId);
-        // 주문 취소
-        order.cancle();
-    }
-
-    public List<Order> findOrders(OrderSearch search) {
-        return orderRepository.findByString(search);
-    }
+  @Transactional
+  public void cancelOrder(Long orderId) {
+    // 주문 엔티티 조회
+    Order order = orderRepository.findOne(orderId);
+    // 주문 취소
+    order.cancle();
+  }
 
 
 
+  public void test() {
+    String start = "";
+    StringBuilder stringBuilder = new StringBuilder();
+    System.out.println(stringBuilder.append(3));
+  }
+
+  public List<Order> findOrders(OrderSearch search) {
+    return orderRepository.findByString(search);
+  }
 }
