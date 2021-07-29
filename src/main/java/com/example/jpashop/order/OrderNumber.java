e37863b70d3b7d6722c3b7acb261b36ab2bf3113
jwoojun <jjwjun10@gmail.com>
2021-07-29 23:21:36 +0900

    VO

---
+++ src/main/java/com/example/jpashop/order/OrderNumber.java
@@ -0,0 +1,10 @@
+package com.example.jpashop.order;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class OrderNumber {
+    public static Object of(int intValue) {
+        return null;
+    }
+}
